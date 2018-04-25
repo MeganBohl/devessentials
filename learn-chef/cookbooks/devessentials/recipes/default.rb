0363720b5ba73971554f87a147ab994ee54e2bb3
Megan Bohl <meganbohl@outlook.com>
2018-04-25 16:01:40 -0500

    push test

---
+++ learn-chef/cookbooks/devessentials/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook:: devessentials
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+#blah
+#blah blah
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+#this is a change to test changing
